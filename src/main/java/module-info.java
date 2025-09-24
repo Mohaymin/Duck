03f37fd578c7dcf1ad4ea45f44497dc7e2766234
Mohaymin <cabdsrijon@gmail.com>
2025-09-24 17:05:20 -0400

    add configuration for adding oracle db

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.oas.sdproject.duck {
     requires javafx.controls;
     requires javafx.fxml;
+    requires spring.boot.autoconfigure;
 
 
     opens com.oas.sdproject.duck to javafx.fxml;
