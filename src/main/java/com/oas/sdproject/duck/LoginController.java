95ba4bc80f7be7a572dfecde95c266a438bd36bf
Mohaymin <cabdsrijon@gmail.com>
2025-10-09 13:56:07 -0400

    add input fields for username, password and a button

--- src/main/java/com/oas/sdproject/duck/LoginController.java
+++ src/main/java/com/oas/sdproject/duck/LoginController.java
@@ -1,13 +1,31 @@
 package com.oas.sdproject.duck;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class LoginController {
 
     @javafx.fxml.FXML
     private ImageView icon;
+    @javafx.fxml.FXML
+    private TextField emailField;
+    @javafx.fxml.FXML
+    private Button loginButton;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
 
     protected ImageView getIcon() {
         return icon;
+    }
+
+    @javafx.fxml.FXML
+    public void onLoginButtonClicked(ActionEvent actionEvent) {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
     }
 }
