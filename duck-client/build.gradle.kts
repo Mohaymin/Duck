0bb61add9e1e4aafb3bcbbe6cc71d854993e8c92
Mohaymin <cabdsrijon@gmail.com>
2025-11-06 08:00:44 -0500

    add module for backend using spring

---
+++ duck-client/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.oas.sdproject"
+version = "0.0.1-SNAPSHOT"
+description = "Spring boot code for the backend of Duck UI"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ duck-client/src/main/java/com/oas/sdproject/DuckClientApplication.java
@@ -0,0 +1,13 @@
+package com.oas.sdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuckClientApplication.class, args);
+    }
+
+}
